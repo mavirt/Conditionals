a273c786622b387dfc59ec50a67e4422cb76ca67
mavirt <65201267+mavirt@users.noreply.github.com>
2020-05-22 13:38:56 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var num1 = prompt("Enter a number");
+var num2 = prompt("Enter another number");
+
+if (num1>0){
+  document.write("The first value is positive. ");
+}
+  else if(num1<0){
+    document.write("The first value is negative. ");
+  }
+    else{
+      document.write("The first value is zero. ");
+    }
+ 
+
+
+  if(num2%2==0){
+  document.write("The second value is even.");
+}
+  else{
+    document.write("The second value is odd.");
+  }
